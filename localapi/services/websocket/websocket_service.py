ce75b74754e95e3aa0dab645c87998a24602c667
BotiCb <boti121103@gmail.com>
2024-11-29 19:25:11 +0200

    Init localapi websocket and mobile photo sending

--- localapi/services/websocket/websocket_service.py
+++ localapi/services/websocket/websocket_service.py
@@ -1,12 +1,17 @@
 import asyncio
 import websockets
-
+import json
 async def echo(websocket, path=None):  # Define a default path argument
     print(f"New connection established. Path: {path}")
     try:
         async for message in websocket:
             print(f"Received: {message}")
-            # await websocket.send(f"Echo: {message}")
+            data = {
+                "type": "training-start",
+                "content": "Hello, how are you?",
+                "sender": "user123"
+            }
+            await websocket.send(json.dumps(data))
     except websockets.ConnectionClosed as e:
         print(f"Connection closed: {e}")
 
