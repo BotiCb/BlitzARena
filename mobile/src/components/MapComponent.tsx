a2f5d8812e3115276c0f2649559ddb887babd1c7
BotiCb <boti121103@gmail.com>
2025-02-25 21:52:57 +0200

    Add marker to user s location

--- mobile/src/components/MapComponent.tsx
+++ mobile/src/components/MapComponent.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import useCoordinates from '~/hooks/useCoordinates';
 
 export const MapComponent = () => {
-  const { location } = useCoordinates({ accuracy: 5 });
+  const { location } = useCoordinates({ accuracy: 5, timeInterval: 1000, distanceInterval: 1 });
+
   useEffect(() => {
     console.log(location);
   }, [location]);
+
   return (
     <View style={[styles.container, { borderWidth: 2, borderColor: 'red' }]}>
       {location && (
@@ -21,14 +23,22 @@ export const MapComponent = () => {
             latitudeDelta: 0.001,
             longitudeDelta: 0.0005,
           }}
-          showsUserLocation
           scrollEnabled
           zoomEnabled
           cameraZoomRange={{
             minCenterCoordinateDistance: 0.1,
             maxCenterCoordinateDistance: 0.01,
-          }}
-        />
+          }}>
+          {/* Marker to show the location */}
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+            title="You are here"
+            description="Current location from useCoordinates"
+          />
+        </MapView>
       )}
       {!PROVIDER_GOOGLE && <Text>Google Maps not available</Text>}
     </View>
